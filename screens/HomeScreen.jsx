63638c5fa59dd0e36de70d6e8b7401f0a8030d86
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2023-08-14 22:30:54 +0200

    initial commit from EXPO INIT

---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+const HomeScreen = () => {
+	return (
+		<SafeAreaView>
+			<Text>HomeScreen</Text>
+		</SafeAreaView>
+	);
+};
+
+export default HomeScreen;
